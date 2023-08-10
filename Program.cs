c06ab912ad00b231064dca73a57e363eaaf18af7
Devran ŞEKER <120737148+devranseker@users.noreply.github.com>
2023-08-10 20:27:28 +0300

    Arrays

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Arrays;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Array ler referans tiplerdir.
+        String[] days = new string[7]
+        {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+        foreach (var day in days)
+        {
+            Console.WriteLine(day);
+        }
+       
+
+
+        Console.WriteLine(days[4]);
+
+        Console.Read();
+    }
+}
+
